4eb614fc732e32bd9b86b668aa3d55b5723a199d
Tzogeo <160930723+Tzogeo@users.noreply.github.com>
2025-08-11 12:38:02 +0300

    Update multi_dimensional_data_to_plot.py

--- multi_dimensional_data_to_plot.py
+++ multi_dimensional_data_to_plot.py
@@ -6,7 +6,7 @@ if __name__=='__main__':
     pt_folder=input("write the relative name of the data folder")#the folder of the data
     parent_folder=input("write the whole name of the parent folder")
     tensor_list,name_list=create_array2(pt_folder,parent_folder)# a list of the input tensors and another with their names
-    while True:
+    while True:#asks the user to pick a dimension reduction method
         method=input("write umap,t-sne or pca to use the corresponding dimension reduction method")
         if method=='t-sne':
             perplexity,embedding=calculate_tsne(tensor_list)
@@ -21,19 +21,15 @@ if __name__=='__main__':
             plot_title,save_title=plot_title("pca",[])
             break
         else: print("the input was invalid")
-    #if random_seed=='Y':embedding=calculate_umap(tensor_list,n_neighbors=n_neighbors,min_dist=min_dist,metric=metric,random_num=False)#returns points in 2-D
-    #else: embedding=calculate_umap(tensor_list,n_neighbors=n_neighbors,min_dist=min_dist,metric=metric,random_num=True)
-    nametotypedict={}#keys: names of proteins, values: the type on the chosen categorization
+
+    nametotypedict={}#keys: names , values: the type on the chosen categorization
     seen = set()
     typetocolordict={}#keys: the types on the chosen categorization, values: the color that corresponds to each type
-    protein_dict={}
-    colors_list=[]
+     colors_list=[]
     chosen_library=input("write plt to save the plot in a .svg file or write plotly to see an interactive plot")
-    #folder_name=input("insert the name of the folder that contains the files of categorization")
-
     
     colorsask=input("write 1 to read from a file with lines on format <<name color type>> 2 to read from a file with only the types and 0 to not show any color")
-    if (input('write 1 if you want as name only the part before the dot or 0 to take the whole name')=='1'):split_bool=True
+    if (input('write 1 if you want as name only the part before the dot or 0 to take the whole name')=='1'):split_bool=True#asks to split the name before a dot. Can be used to organise the data into larger categories
     else: split_bool=False
 
     if colorsask=='1':
@@ -47,11 +43,10 @@ if __name__=='__main__':
         colors_list=['#663399' for _ in range(len(name_list))]
         typetocolordict={"all":"#663399"}
         for name in name_list: nametotypedict[name.split(".")[0]]="all"
-    #wherecolors=input("write 0 to take the colors from a different file, 1 to pick a couple of")
     
     markers_bool=input("write 1 to have markers or 0 for no markers")
     if markers_bool=='1':
-        marker_folder=input('copy the relative path to the folder with the different .txt files')
+        marker_folder=input('copy the whole path to the folder with the different .txt files')
     name_set=set(name_list)
 
     name_dict={}
@@ -64,12 +59,12 @@ if __name__=='__main__':
         if chosen_library=='plotly':#for the interactive plot using plotly
             markers,marker_dict=markers_from_many_files_plotly(marker_folder,name_list)#finds the markers
             color_and_marker_plot_to_plotly(name_list=name_list,markers_list=markers,embedding=embedding,plot_title=plot_title,marker_dict=marker_dict,nametotypedict=nametotypedict,typetocolordict=typetocolordict)
-        elif chosen_library=='plt':
+        elif chosen_library=='plt':#to save the plot in a svg file using matplotlib.pyplot
             markers,marker_dict=markers_from_many_files_plt(marker_folder,name_list)
             color_and_marker_plot_to_plt(name_list,markers,marker_dict,embedding,plot_title,nametotypedict,typetocolordict,save_title,parent_folder)
     else:
         if not colors_list:colors_list=dict_to_color_list(name_list,typetocolordict,nametotypedict)#if the list is empty
         if chosen_library=='plotly':#for the interactive plot using plotly
             color_plot_to_plotly(name_list=name_list,colors_list=colors_list,embedding=embedding,plot_title=plot_title,typetocolordict=typetocolordict)
-        elif chosen_library=='plt':
+        elif chosen_library=='plt':#to save the plot in a svg file using matplotlib.pyplot
             color_plot_to_plt(colors_list=colors_list,embedding=embedding,plot_title=plot_title,typetocolordict=typetocolordict,save_title=save_title,parent_folder=parent_folder)      
